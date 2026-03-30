5a1aaabd6f29c66b56d07b9e16d70dba05f58fa1
3oman <143084228+3oman@users.noreply.github.com>
2026-03-30 22:07:43 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/Module4')
+def module4():
+    return render_template('Module4.html')
+
+
+if __name__ == '__main__':    app.run(debug=True)
